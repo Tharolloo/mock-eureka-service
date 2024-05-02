75537054ca82f087bd0fa5df8c684f64d886d298
samuelshola777 <samuelshola14@gmail.com>
2024-05-02 01:26:47 +0100

    eureca server

---
+++ src/main/java/eureka/service/mock/eureka/service/MockEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package eureka.service.mock.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockEurekaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eureka/service/mock/eureka/service/MockEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eureka.service.mock.eureka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
